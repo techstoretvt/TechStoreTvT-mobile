45f33db4e4333bc04c708f51087de59b7120d16a
techstoretvt <techstoretvt2565@gmail.com>
2023-07-31 18:03:52 +0700

    v23.7.31

--- src/screens/detail_product_screen/detail_product_screen.tsx
+++ src/screens/detail_product_screen/detail_product_screen.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, Image, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import React from 'react';
 import ImageView from 'react-native-image-viewing';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Swiper from 'react-native-swiper';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Badge } from '@rneui/themed';
+import { Badge, AirbnbRating } from '@rneui/themed';
+import RenderHtml from 'react-native-render-html';
 
 import styles from './detail_product_styles';
 import FocusAwareStatusBar from '../../components/FocusAwareStatusBar/FocusAwareStatusBar';
@@ -32,63 +33,215 @@ const DetailProductScreen = () => {
   const insets = useSafeAreaInsets();
 
   const [openLightBox, setOpenLightBox] = React.useState(false);
+
   return (
-    <ScrollView>
-      <View style={styles.DetailProduct_container}>
-        <FocusAwareStatusBar translucent backgroundColor="transparent" />
-        <View style={[styles.header, { paddingTop: insets.top }]}>
-          <View style={styles.header_goBack}>
-            <Icon name="arrow-left" size={20} color="#fff" />
-          </View>
-          <View style={styles.header_wrapIcon}>
-            <View style={styles.header_wrapIcon_icon}>
-              <Icon name="share" size={20} color="#fff" />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      enabled={true}
+      keyboardVerticalOffset={10}
+    >
+      <ScrollView>
+        <View style={styles.DetailProduct_container}>
+          <FocusAwareStatusBar translucent backgroundColor="transparent" />
+          <View style={[styles.header, { paddingTop: insets.top }]}>
+            <View style={styles.header_goBack}>
+              <Icon name="arrow-left" size={20} color="#fff" />
             </View>
-            <View style={styles.header_wrapIcon_icon}>
-              <Icon name="cart-plus" size={26} color="#fff" />
-              <Badge
-                value={10}
-                status="error"
-                // eslint-disable-next-line react-native/no-inline-styles
-                containerStyle={{
-                  position: 'absolute',
-                  top: -6,
-                  right: -8,
+            <View style={styles.header_wrapIcon}>
+              <View style={styles.header_wrapIcon_icon}>
+                <Icon name="share" size={20} color="#fff" />
+              </View>
+              <View style={styles.header_wrapIcon_icon}>
+                <Icon name="cart-plus" size={26} color="#fff" />
+                <Badge
+                  value={10}
+                  status="error"
+                  // eslint-disable-next-line react-native/no-inline-styles
+                  containerStyle={{
+                    position: 'absolute',
+                    top: -6,
+                    right: -8,
+                  }}
+                />
+              </View>
+            </View>
+          </View>
+          <View style={[styles.wrapImages]}>
+            <Swiper
+              showsButtons={false}
+              horizontal={true}
+              dotColor="#fff"
+              showsPagination={true}
+              // eslint-disable-next-line react-native/no-inline-styles
+              paginationStyle={{
+                bottom: 4,
+              }}
+              loop={false}
+            >
+              <Image source={{ uri: 'https://source.unsplash.com/random?sig=1' }} style={styles.wrapImages_image} />
+              <Image source={{ uri: 'https://source.unsplash.com/random?sig=2' }} style={styles.wrapImages_image} />
+              <Image source={{ uri: 'https://source.unsplash.com/random?sig=3' }} style={styles.wrapImages_image} />
+            </Swiper>
+          </View>
+          <View style={styles.content}>
+            <View style={styles.content_nameProduct}>
+              <Text style={styles.content_nameProduct_text}>
+                Ten san pham la abc Ten san pham la abc Ten san pham la abc Ten san pham la abc Ten san
+              </Text>
+            </View>
+            <View style={styles.content_wrapStatus}>
+              <View style={styles.content_status}>
+                <Text style={styles.content_status_title}>Tình trạng:</Text>
+                <Text style={styles.content_status_content}>Đang bán</Text>
+              </View>
+              <View style={styles.content_wrapStatus_separate} />
+              <View style={styles.content_trademark}>
+                <Text style={styles.content_trademark_title}>Thương hiệu:</Text>
+                <Text style={styles.content_trademark_content}>Asus</Text>
+              </View>
+            </View>
+            <View style={styles.content_evaluate}>
+              {/* <View style={styles.content_evaluate_empty}>
+              <Text style={styles.content_evaluate_empty_text}>Chưa có đánh giá</Text>
+            </View> */}
+              <View style={styles.content_evaluate_wrap}>
+                <View style={styles.content_evaluate_wrap_star}>
+                  <Text style={styles.content_evaluate_wrap_star_text}>4.5</Text>
+                  <View style={styles.content_evaluate_wrap_star_icon}>
+                    <AirbnbRating isDisabled={true} defaultRating={4} size={16} showRating={false} />
+                  </View>
+                </View>
+                <View style={styles.content_wrapStatus_separate} />
+                <View style={styles.content_evaluate_wrap_text}>
+                  <Text style={styles.content_evaluate_wrap_text_number}>5</Text>
+                  <Text style={styles.content_evaluate_wrap_text_title}>Đánh giá</Text>
+                </View>
+                <View style={styles.content_wrapStatus_separate} />
+                <View style={styles.content_evaluate_wrap_text}>
+                  <Text style={styles.content_evaluate_wrap_text_number}>5</Text>
+                  <Text style={styles.content_evaluate_wrap_text_title}>Đã bán</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.content_price}>
+              <View style={styles.content_price_current}>
+                <Text style={styles.content_price_current_text}>d400.000</Text>
+              </View>
+              <View style={styles.content_priceRoot}>
+                <Text style={styles.content_priceRoot_price}>d800.000</Text>
+                <Text style={styles.content_priceRoot_persent}>-50%</Text>
+              </View>
+            </View>
+            <View style={styles.content_classify}>
+              <View style={styles.content_classify_left}>
+                <Text style={styles.content_classify_left_title}>Phân loại:</Text>
+                <Text style={styles.content_classify_left_amount}>Kho: 30</Text>
+              </View>
+              <View style={styles.content_classify_right}>
+                <View style={styles.content_classify_right_item}>
+                  <Text style={styles.content_classify_right_item_text}>Xanh</Text>
+                </View>
+                <View style={styles.content_classify_right_item}>
+                  <Text style={styles.content_classify_right_item_text}>Xanh</Text>
+                </View>
+                <View style={styles.content_classify_right_item}>
+                  <Text style={styles.content_classify_right_item_text}>Xanh</Text>
+                </View>
+                <View style={styles.content_classify_right_item}>
+                  <Text style={styles.content_classify_right_item_text}>Xanh</Text>
+                </View>
+                <View style={styles.content_classify_right_item}>
+                  <Text style={styles.content_classify_right_item_text}>Xanh</Text>
+                </View>
+                <View style={styles.content_classify_right_item}>
+                  <Text style={styles.content_classify_right_item_text}>Xanh</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.introduces}>
+            <View style={styles.introduces_wrapText}>
+              <Text style={styles.introduces_wrapText_title}>Chính sách bán hàng</Text>
+              <View style={styles.introduces_wrapText_listItem}>
+                <View style={styles.introduces_wrapText_listItem_item}>
+                  <Image
+                    source={require('../../assets/images/DetailProduct/icon-1.webp')}
+                    style={styles.introduces_wrapText_listItem_item_image}
+                  />
+                  <Text style={styles.introduces_wrapText_listItem_item_text}>Cam kết 100% chính hãng</Text>
+                </View>
+                <View style={styles.introduces_wrapText_listItem_item}>
+                  <Image
+                    source={require('../../assets/images/DetailProduct/icon-2.webp')}
+                    style={styles.introduces_wrapText_listItem_item_image}
+                  />
+                  <Text style={styles.introduces_wrapText_listItem_item_text}>Miễn phí giao hàng</Text>
+                </View>
+                <View style={styles.introduces_wrapText_listItem_item}>
+                  <Image
+                    source={require('../../assets/images/DetailProduct/icon-3.webp')}
+                    style={styles.introduces_wrapText_listItem_item_image}
+                  />
+                  <Text style={styles.introduces_wrapText_listItem_item_text}>Hỗ trợ 24/7</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.introduces_wrapText}>
+              <Text style={styles.introduces_wrapText_title}>Thông tin thêm</Text>
+              <View style={styles.introduces_wrapText_listItem}>
+                <View style={styles.introduces_wrapText_listItem_item}>
+                  <Image
+                    source={require('../../assets/images/DetailProduct/icon-4.webp')}
+                    style={styles.introduces_wrapText_listItem_item_image}
+                  />
+                  <Text style={styles.introduces_wrapText_listItem_item_text}>Hoàn tiền 111% nếu hàng giã</Text>
+                </View>
+                <View style={styles.introduces_wrapText_listItem_item}>
+                  <Image
+                    source={require('../../assets/images/DetailProduct/icon-5.webp')}
+                    style={styles.introduces_wrapText_listItem_item_image}
+                  />
+                  <Text style={styles.introduces_wrapText_listItem_item_text}>Mở hợp kiểm tra nhận hàng</Text>
+                </View>
+                <View style={styles.introduces_wrapText_listItem_item}>
+                  <Image
+                    source={require('../../assets/images/DetailProduct/icon-6.webp')}
+                    style={styles.introduces_wrapText_listItem_item_image}
+                  />
+                  <Text style={styles.introduces_wrapText_listItem_item_text}>Đổi trả trong 7 ngày</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.discription}>
+            <Text style={styles.discription_title}>Mô tả sản phẩm</Text>
+            <View style={styles.discription_content}>
+              <Text>mo ta san pham</Text>
+              <RenderHtml
+                contentWidth={300}
+                source={{
+                  html: `
+      <p style='text-align:center;'>
+        Hello World!
+      </p>`,
                 }}
               />
             </View>
           </View>
+
+          <ImageView
+            images={images}
+            imageIndex={2}
+            visible={openLightBox}
+            onRequestClose={() => setOpenLightBox(false)}
+            presentationStyle="overFullScreen"
+            // backgroundColor="yellow"
+            // eslint-disable-next-line react/no-unstable-nested-components, react-native/no-inline-styles
+            FooterComponent={() => <Text style={{ color: '#fff' }}>1/6</Text>}
+          />
         </View>
-        <View style={[styles.wrapImages]}>
-          <Swiper
-            showsButtons={false}
-            horizontal={true}
-            dotColor="#fff"
-            showsPagination={true}
-            // eslint-disable-next-line react-native/no-inline-styles
-            paginationStyle={{
-              bottom: 4,
-            }}
-            loop={false}
-          >
-            <Image source={{ uri: 'https://source.unsplash.com/random?sig=1' }} style={styles.wrapImages_image} />
-            <Image source={{ uri: 'https://source.unsplash.com/random?sig=2' }} style={styles.wrapImages_image} />
-            <Image source={{ uri: 'https://source.unsplash.com/random?sig=3' }} style={styles.wrapImages_image} />
-          </Swiper>
-        </View>
-        <Text>DetailProductScreen</Text>
-        <ImageView
-          images={images}
-          imageIndex={2}
-          visible={openLightBox}
-          onRequestClose={() => setOpenLightBox(false)}
-          presentationStyle="overFullScreen"
-          // backgroundColor="yellow"
-          // eslint-disable-next-line react/no-unstable-nested-components, react-native/no-inline-styles
-          FooterComponent={() => <Text style={{ color: '#fff' }}>1/6</Text>}
-        />
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- src/screens/detail_product_screen/detail_product_styles.js
+++ src/screens/detail_product_screen/detail_product_styles.js
@@ -4,7 +4,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   DetailProduct_container: {
     flex: 1,
-    backgroundColor: 'aqua',
+    // backgroundColor: 'aqua',
   },
   header: {
     position: 'absolute',
@@ -44,6 +44,178 @@ const styles = StyleSheet.create({
   },
   wrapImages_image: {
     height: '100%',
+  },
+  content: {
+    padding: 6,
+    backgroundColor: '#fff',
+    marginTop: 10,
+  },
+  content_nameProduct: {},
+  content_nameProduct_text: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#000',
+  },
+  content_wrapStatus: {
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    // justifyContent: 'space-between',
+  },
+  content_wrapStatus_separate: {
+    width: 1,
+    height: 16,
+    borderLeftWidth: 1,
+    marginHorizontal: 10,
+  },
+  content_status: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content_status_title: {},
+  content_status_content: {
+    color: 'orange',
+    fontWeight: '800',
+    fontSize: 15,
+    marginLeft: 10,
+  },
+  content_trademark: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content_trademark_title: {},
+  content_trademark_content: {
+    color: 'orange',
+    fontWeight: '800',
+    fontSize: 15,
+    marginLeft: 10,
+  },
+  content_evaluate: {},
+  content_evaluate_empty: {},
+  content_evaluate_empty_text: {
+    color: '#333',
+  },
+  content_evaluate_wrap: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content_evaluate_wrap_star: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content_evaluate_wrap_star_text: {
+    color: '#000',
+    textDecorationLine: 'underline',
+  },
+  content_evaluate_wrap_star_icon: {
+    marginLeft: 4,
+  },
+  content_evaluate_wrap_text: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content_evaluate_wrap_text_number: {
+    color: '#000',
+    textDecorationLine: 'underline',
+  },
+  content_evaluate_wrap_text_title: {
+    marginLeft: 4,
+  },
+  content_price: {
+    marginTop: 10,
+  },
+  content_price_current: {},
+  content_price_current_text: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: 'red',
+  },
+  content_priceRoot: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  content_priceRoot_price: {
+    textDecorationLine: 'line-through',
+  },
+  content_priceRoot_persent: {
+    marginLeft: 6,
+    borderWidth: 1,
+    borderColor: 'red',
+    color: 'red',
+    paddingHorizontal: 4,
+  },
+  content_classify: {
+    flexDirection: 'row',
+    marginTop: 14,
+  },
+  content_classify_left: {},
+  content_classify_left_title: {
+    color: '#000',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  content_classify_left_amount: {},
+  content_classify_right: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginLeft: 14,
+    flexWrap: 'wrap',
+  },
+  content_classify_right_item: {
+    borderWidth: 1,
+    borderColor: 'orange',
+    paddingHorizontal: 6,
+    paddingVertical: 4,
+    margin: 4,
+  },
+  content_classify_right_item_text: {
+    color: 'orange',
+  },
+  introduces: {
+    backgroundColor: '#fff',
+    marginTop: 10,
+    paddingHorizontal: 6,
+  },
+  introduces_wrapText: {
+    marginTop: 20,
+  },
+  introduces_wrapText_title: {
+    fontWeight: '700',
+    fontSize: 17,
+    color: '#000',
+  },
+  introduces_wrapText_listItem: {
+    marginTop: 3,
+    marginLeft: 10,
+  },
+  introduces_wrapText_listItem_item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 4,
+  },
+  introduces_wrapText_listItem_item_image: {
+    width: 20,
+    height: 20,
+  },
+  introduces_wrapText_listItem_item_text: {
+    marginLeft: 10,
+    color: '#000',
+  },
+  discription: {
+    marginTop: 10,
+    backgroundColor: '#fff',
+    paddingHorizontal: 6,
+    paddingVertical: 10,
+  },
+  discription_title: {
+    fontWeight: '700',
+    fontSize: 18,
+    color: '#000',
+    textDecorationLine: 'underline',
+  },
+  discription_content: {
+    marginTop: 10,
   },
 });
 
